e8db382b2553b5e6ded68778fcb165a91270ea3d
ntababi01 <ntabbabi01@gmail.com>
2022-03-02 22:55:25 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import{ Form } from 'react-bootstrap';
+import{ Button } from 'react-bootstrap'
+function App() {
+  return (
+    <div className="app">
+      <Form>
+  <fieldset disabled>
+    <Form.Group className="mb-3">
+      <Form.Label htmlFor="disabledTextInput">Disabled input</Form.Label>
+      <Form.Control id="disabledTextInput" placeholder="Disabled input" />
+    </Form.Group>
+    <Form.Group className="mb-3">
+      <Form.Label htmlFor="disabledSelect">Disabled select menu</Form.Label>
+      <Form.Select id="disabledSelect">
+        <option>Disabled select</option>
+      </Form.Select>
+    </Form.Group>
+    <Form.Group className="mb-3">
+      <Form.Check
+        type="checkbox"
+        id="disabledFieldsetCheck"
+        label="Can't check this"
+      />
+    </Form.Group>
+    <Button type="submit">Submit</Button>
+  </fieldset>
+</Form>
+    </div>
+  );
+}
+
+export default App;
